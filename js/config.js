6e0275367caa666e5ed67ea6f03ed11c084017cc
huashetian <2072662656M@gmail.com>
2025-09-07 16:26:17 +0800

    更新 config.js

--- js/config.js
+++ js/config.js
@@ -114,10 +114,6 @@ const API_SITES = {
         api: 'https://cj.lziapi.com/api.php/provide/vod/',
         name: '量子资源站'
     },
-   wangerxiaofangniuwa: {
-        api: 'http://tvbox.王二小放牛娃.top/',
-        name: '王二小放牛娃',
-},
     testSource: {
         api: 'https://www.example.com/api.php/provide/vod',
         name: '空内容测试源',
